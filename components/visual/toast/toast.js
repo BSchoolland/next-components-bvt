ee06b6a1f9809036fb5aa3416e964f509ba6db38
Benjamin Schoolland <bschoolland@gmail.com>
2024-05-16 21:04:02 -0700

    feat: fix styles

--- components/visual/toast/toast.js
+++ components/visual/toast/toast.js
@@ -1,20 +1,23 @@
-import styles from "./toast.module.css";
+import React, {createRef} from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-import { useToasts } from './toastProvider';
+import { useToasts } from "./toastProvider";
+import "./toast.css";
 
 
 const Toast = () => {
   const { toasts, removeToast } = useToasts();
   return (
-    <TransitionGroup className={styles.toastContainer}>
-      {toasts.map(toast => (
-        <CSSTransition key={toast.id} timeout={1000} classNames={styles.toast}>
-          <div className={styles.toast} onClick={() => removeToast(toast.id)}>
+    <TransitionGroup className="toast-wrapper">
+      {toasts.map((toast) => {
+        const nodeRef = createRef();
+        return (
+          <CSSTransition key={toast.id} timeout={3000} classNames="toast" nodeRef={nodeRef}>
+          <div ref={nodeRef} className="toast" onClick={() => removeToast(toast.id)}>
             {toast.content}
           </div>
         </CSSTransition>
-      ))}
-  </TransitionGroup>
+      )})}
+    </TransitionGroup>
   );
 };
 
